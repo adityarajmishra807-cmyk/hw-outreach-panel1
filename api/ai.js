e47a5355572887502ce2beef0a3ad0a27fff27f9
Aditya Raj Mishra <adityarajmishra807@gmail.com>
2026-09-12 16:11:05 +0530

    fix: make Gemini function compatible with ESM

--- api/ai.js
+++ api/ai.js
@@ -1,4 +1,4 @@
-const { buildContext } = require('./lib/context');
+import { buildContext } from './lib/context.js';
 
 export default async function handler(req, res) {
   if (req.method !== 'POST') {
